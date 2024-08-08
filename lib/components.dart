efc4125ca74903906e893d03d0a77c6eefafde3a
htomeload <ko_player_555@outlook.com>
2024-08-08 16:11:37 +0700

    Remove unused import and variables

--- lib/components.dart
+++ lib/components.dart
@@ -2,7 +2,6 @@ import 'package:budget_app_starting/view_model.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:sign_button/sign_button.dart';
--- lib/web/login_view_web.dart
+++ lib/web/login_view_web.dart
@@ -1,17 +1,11 @@
 import 'package:budget_app_starting/components.dart';
 import 'package:budget_app_starting/view_model.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:sign_button/sign_button.dart';
 
 class LoginViewWeb extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final viewModelProvider = ref.watch(viewModel);
     double deviceWidth = MediaQuery.of(context).size.width;
     double deviceHeight = MediaQuery.of(context).size.height;
 
